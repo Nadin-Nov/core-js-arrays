64e9f8966c0de4de0a5d08267e934c807e5d6d90
Nadin Novoselova <nadja.novoselova@gmail.com>
2024-12-28 00:11:00 +0300

    refactor: add getTail function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -204,8 +204,9 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  const lenArr = arr.length;
+  return arr.slice(lenArr - n, lenArr);
 }
 
 /**
