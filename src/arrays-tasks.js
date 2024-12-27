025d0d6cba5e8898af0fdeaca5f538277d678055
Nadin Novoselova <nadja.novoselova@gmail.com>
2024-12-27 23:34:35 +0300

    refactor: add isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -143,8 +143,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const newArr = arr.map((str) => str.length);
+  return newArr.every((element) => element === newArr[0]);
 }
 
 /**
