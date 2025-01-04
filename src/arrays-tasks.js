132c44d1118e405d0829431b42a0a894926c4c4a
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-01-04 23:55:09 +0300

    refactor: add getMaxItems function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -465,8 +465,8 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((x1, x2) => x2 - x1).slice(0, n);
 }
 
 /**
